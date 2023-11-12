72b8018234b17cb83c5622bb47bc4d67b44a91b6
BarbodH <habibi.k.barbod@gmail.com>
2023-11-12 00:54:55 -0500

    Debugged choose passage filter topic functionality

--- knowledge_box/generate_questions/static/script.js
+++ knowledge_box/generate_questions/static/script.js
@@ -1,8 +1,7 @@
 const buttonApplyFilters = document.getElementById("choose_passages_button_apply_filters");
 const buttonSelectAll = document.getElementById("choose_passages_button_select_all");
 const buttonDeselectAll = document.getElementById("choose_passages_button_deselect_all");
-const tablePassages = document.getElementById("choose-passages-table-passages");
-const tableRowsPassages = tablePassages.getElementsByTagName("tr");
+const passageList = document.getElementById("passage-list");
 
 buttonApplyFilters.addEventListener("click", () => {
     const checkboxes = document.querySelectorAll("input[type='checkbox'][name='topics']")
@@ -12,12 +11,14 @@ buttonApplyFilters.addEventListener("click", () => {
         topicCheckboxes[checkbox.value] = checkbox.checked;
     });
 
-    for (let i = 0; i < tableRowsPassages.length; i++) {
-        const row = tableRowsPassages[i];
-        const topic = row.getAttribute("topic");
-        
-        if (topicCheckboxes[topic]) row.classList.remove("hide");
-        else row.classList.add("hide");
+    const passageItems = passageList.querySelectorAll("li");
+
+    for (let i = 0; i < passageItems.length; i++) {
+        const passageItem = passageItems[i];
+        const topic = passageItem.getAttribute("topic");
+
+        if (topicCheckboxes[topic]) passageItem.classList.remove("hide");
+        else passageItem.classList.add("hide");
     }
 });
 
